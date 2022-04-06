6c589a1dcfbe967a91443a98c87cdc5c1986d4b2
magnus.wielje@live.dk <71823140+magnusGeden@users.noreply.github.com>
2022-04-06 15:31:42 +0200

    Gay Anton hjælp
    Døden er nør

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GridManager : MonoBehaviour
 {
     [SerializeField] private int _width, _height;
@@ -10,13 +10,19 @@ public class GridManager : MonoBehaviour
 
     [SerializeField] private Transform _cam;
 
+    private Dictionary<Vector2, Tile> _tiles;
+
+    public GameObject Bg;
+
     private void Start()
     {
+        Bg = GameObject.Find("GridBg");
         GenerateGrid();
     }
 
     void GenerateGrid()
     {
+        _tiles = new Dictionary<Vector2, Tile>();
         for (int x=0; x < _width; x++)
         {
             for (int y = 0; y < _height; y++)
@@ -26,11 +32,24 @@ public class GridManager : MonoBehaviour
 
                 var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
                 spawnedTile.Init(isOffset);
+
+                _tiles[new Vector2(x,y)] = spawnedTile;
             }
         }
 
 
         _cam.transform.position = new Vector3((float)_width/2-0.5f, (float)_height / 2 - 0.5f,-10);
+        Bg.transform.position = new Vector3((float)_width/2-0.5f , (float)_height/2-0.5f);
+        Bg.transform.localScale = new Vector3((float)_width , (float)_height);
+    }
+
+    public Tile GetTileAtPosition(Vector2 pos)
+    {
+        if(_tiles.TryGetValue(pos, out var tile))
+        {
+            return tile;
+        }
+        return null;
     }
 
 }
---
+++ Assets/Tiles/Tile.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.U2D;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] private Color _baseColor, _offsetColor;
+    [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private GameObject _highlight;
+
+    public void Init(bool isOffset)
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+        _renderer.color = isOffset ? _offsetColor : _baseColor;
+    }
+
+    private void OnMouseEnter()
+    {
+        _highlight.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        _highlight.SetActive(false);
+    }
+
+
+
+}
