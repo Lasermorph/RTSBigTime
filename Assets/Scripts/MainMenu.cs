e3ab3825c1de16b2c2a55c54c8c5e75748b5da37
magnusGeden <magnus.wielje@live.dk>
2022-04-19 21:25:52 +0200

    MenuTest
    Har lavet en menu, dog mangler jeg at kode det sidste, eftersom vi kører en ældre version, er der nogen problemmer

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneMangagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string firstLevet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(firstLevel);
+    }
+
+    public void OpenOptions()
+    {
+
+    }
+
+    public void CloseOptions()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quitting");
+    }
+
+}
