5bdaa500692a4c750d9328cc3c3787bd74783767
magnus.wielje@live.dk <71823140+magnusGeden@users.noreply.github.com>
2022-04-29 13:01:28 +0200

    ResWork
    Doing resolution

--- Assets/Scripts/OptionsScreen.cs
+++ Assets/Scripts/OptionsScreen.cs
@@ -5,7 +5,13 @@ using UnityEngine.UI;
 
 public class OptionsScreen : MonoBehaviour
 {
+    
     public Toggle fullscreenTog, vsyncTog;
+
+    public List<resItem> resulutions = new List<resItem>();
+
+    private int selectedResolution;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -30,9 +36,21 @@ public class OptionsScreen : MonoBehaviour
 
     }
 
+
+    public void ResLeft()
+    {
+
+    }
+
+    public void ResRight()
+    {
+
+    }
+
+
     public void ApplyGraphics()
     {
-        OptionsScreen.fullScreen = fullsreenTog.isOn;
+        Screen.fullScreen = fullscreenTog.isOn;
 
         if (vsyncTog.isOn)
         {
@@ -44,6 +62,12 @@ public class OptionsScreen : MonoBehaviour
         }
 
     }
+    [System.Serializable]
+    public class resItem 
+    {
 
+        public int horizontal, vertical;
+
+    }
 
 }
