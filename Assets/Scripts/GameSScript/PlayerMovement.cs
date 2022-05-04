b758b193f73c5b887d6c85bdecfd3421d63b1e82
Lasermorph <stefanashansen@hotmail.com>
2022-05-04 14:29:42 +0200

    something

--- Assets/Scripts/GameSScript/PlayerMovement.cs
+++ Assets/Scripts/GameSScript/PlayerMovement.cs
@@ -13,7 +13,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update () {
  
-        if(Input.GetKeyDown(KeyCode.Mouse0))
+        if(Input.GetKeyDown(KeyCode.Mouse1))
            {
             targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
